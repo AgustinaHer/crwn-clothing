2145b81101c7124264d138b74bcd33d86efdbf3f
Agustina <agustina@gogrow.dev>
2021-03-11 08:27:46 -0300

    implemented firebase utils, included ability to store authenticated user into a firestore database

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Header from './components/header/header.component';
 
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 
 class App extends React.Component {
@@ -25,10 +25,21 @@ constructor(){
   unsubscribeFromAuth = null
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+      }
 
-      console.log(user);
+      this.setState({currentUser:userAuth});
     });
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,8 +3,7 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const config =
-    {
+const config = {
         apiKey: "AIzaSyBYhicW53NQ_2RoYqg8S9ysaL-ONxvv6qs",
         authDomain: "crwn-db-1a980.firebaseapp.com",
         projectId: "crwn-db-1a980",
@@ -12,7 +11,33 @@ const config =
         messagingSenderId: "993438986757",
         appId: "1:993438986757:web:2bf799174b1994af6b226c",
         measurementId: "G-9TXH29FVBZ"
-      }; 
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if(!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if(!snapShot.exists){
+    const {displayName, email} = userAuth;
+    const createdAt = new Date();
+
+    try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        })
+    } catch(error){
+        console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
 
 firebase.initializeApp(config);
 
